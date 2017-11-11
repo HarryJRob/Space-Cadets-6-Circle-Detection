4c7d28d5e158950b26b1644a3a0b5e4a34686962
Unknown <harryjrobertson@gmail.com>
2017-11-11 16:45:20 +0000

    Removed redundencies

--- src/Circle_Detection.java
+++ src/Circle_Detection.java
@@ -1,15 +1,12 @@
 import java.awt.image.BufferedImage;
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
 public class Circle_Detection extends Application {
@@ -19,32 +16,17 @@ public class Circle_Detection extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
 			ImageView iView1 = new ImageView();
 			ImageView iView2 = new ImageView();
-			ImageView iView3 = new ImageView();
 			ImageView iView4 = new ImageView();
 			
 			BufferedImage unfilteredImage = imageFunc.applyGaussianBlur(imageFunc.applyGaussianBlur(imageFunc.applyGaussianBlur(imageFunc.loadImage(imgDir))));
 			
 			iView1.setImage(bufferedImgToImg(unfilteredImage));
 			
-			root.setTop(iView1);
-			root.setLeft(iView2);
-			root.setRight(iView3);
-			root.setBottom(iView4);
-			
-			Rectangle2D psb = Screen.getPrimary().getVisualBounds();
-			Scene scene = new Scene(root,psb.getWidth(),psb.getHeight());
-			
 			BufferedImage grayImage = imageFunc.bufferedImagetoGrayScale(unfilteredImage);
 			
-			
 			iView2.setImage(bufferedImgToImg(grayImage));
-			
-			BufferedImage sobelImage = imageFunc.applySobelOperator(grayImage);
-			
-			iView3.setImage(bufferedImgToImg(sobelImage));
 			
 			BufferedImage houghImage = imageFunc.applySobelHough(grayImage, 10, 30,3);
 			
--- src/imageFunc.java
+++ src/imageFunc.java
@@ -94,144 +94,6 @@ public abstract class imageFunc {
 	    return outputImage;
 	}
 	
-	//Apply Sobel Operator to a BufferedImage
-	public static BufferedImage applySobelOperator(BufferedImage img) {
-		
-		//Get the width and height of the image
-		int width = img.getWidth();
-		int height = img.getHeight();
-		
-		//Create a image to output to
-		BufferedImage outputImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-		
-		//For each pixel do
-		//This avoids the edge of the image completely.
-		//In a megapixel wide image the edge pixels make up a tiny amount of image and so can be safely ignored
-		for(int y = 1; y < height-1; y++) {
-			for(int x = 1; x < width-1; x++) {
-				int[][] pixelMatrix = new int[3][3];
-                pixelMatrix[0][0]=img.getRGB(x-1,y-1)&0xff;
-                pixelMatrix[0][1]=img.getRGB(x-1,y)&0xff;
-                pixelMatrix[0][2]=img.getRGB(x-1,y+1)&0xff;
-                
-                pixelMatrix[1][0]=img.getRGB(x,y-1)&0xff;
-                pixelMatrix[1][2]=img.getRGB(x,y+1)&0xff;
-                
-                pixelMatrix[2][0]=img.getRGB(x+1,y-1)&0xff;
-                pixelMatrix[2][1]=img.getRGB(x+1,y)&0xff;
-                pixelMatrix[2][2]=img.getRGB(x+1,y+1)&0xff;
-				
-    			int gx=(pixelMatrix[0][0]*1) + (pixelMatrix[0][1]*2) + (pixelMatrix[0][2]*1)+
-    					(pixelMatrix[2][0]*-1) + (pixelMatrix[2][1]*-2) + (pixelMatrix[2][2]*-1);
-                
-        		int gy=(pixelMatrix[0][0]*1) + (pixelMatrix[1][0]*2) + (pixelMatrix[2][0]*1)+
-        				(pixelMatrix[0][2]*-1) + (pixelMatrix[1][2]*-2) + (pixelMatrix[2][2]*-1);
-
-                int g = (int) Math.sqrt(Math.pow(gy,2)+Math.pow(gx,2));
-                
-                outputImage.setRGB(x,y,(g<<16|g<<8|g));
-			}
-		}
-		return outputImage;
-	}
-
-//	//Apply Hough Transform to an image
-//	public static BufferedImage applyHoughTransform(BufferedImage img, int minRadius, int maxRadius, int numberOfCircles) {
-//		
-//		int width = img.getWidth();
-//		int height = img.getHeight();
-//		int[][][] accumulator = new int[width][height][maxRadius];
-//		
-//		//Do the Hough calculations
-//		{
-//			final int incrementRadius = 1;
-//			
-//			int averageColor = findAverageColour(img).getBlue();
-//			
-//			//For each pixel
-//			for(int x = 0; x < width; x++ ) {
-//				for(int y = 0; y < height; y++) {
-//					//Where the colour of the pixel is greater than average
-//					if((img.getRGB(x, y)&0xff) > averageColor) {
-//					    //For each radius
-//						for(int radius = minRadius; radius < maxRadius; radius+=incrementRadius) {
-//							//For each angle
-//							for(int theta = 0; theta < 360; theta++) {
-//								//Calculate the middle coordinates of the circle and add them to the accumulator
-//								int a = (int) (x - radius * Math.cos(theta * Math.PI/180));
-//								int b = (int) (y - radius * Math.sin(theta * Math.PI/180));
-//									
-//								if((a >= 0 && a < width) && (b >= 0 && b < height)) {
-//									accumulator[a][b][radius] += 1;
-//								}
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//		
-//		//Calculate the average Vote
-//		int averageVote = 0;
-//		
-//		for(int x = 0; x < width; x++) {
-//			for(int y = 0; y < height; y++) {
-//				for(int r = minRadius; r < maxRadius; r++) {
-//					averageVote += accumulator[x][y][r];
-//				}
-//			}
-//		}
-//		
-//		averageVote /= width*height*(maxRadius - minRadius);
-//		
-//		LinkedList<Circle> circleList = new LinkedList<Circle>();
-//		
-//		//If the vote is above average and is the highest compared to it's neighbours
-//		for(int r = minRadius; r < maxRadius; r++ ) {
-//			for(int x = 0; x < width; x++) {
-//				for(int y = 0; y < height; y++) {
-//					int curVote = accumulator[x][y][r];
-//					if(curVote > 1.5f*averageVote && houghTestMaxima(accumulator,x,y,r,curVote)) {
-//						circleList.add(new Circle(x,y,r));
-//					}
-//				}
-//			}
-//		}
-//		
-//		//Get the highest priority circles defined by the numberOfCircles variable 
-//		LinkedList<Circle> finalCircleList = new LinkedList<Circle>();
-//		
-//		for(int i = 0; i < numberOfCircles; i++) {
-//			if(circleList.size() != 0) {
-//				Circle highestPriorityCircle = getHighestPriorityCircle(circleList);
-//				finalCircleList.add(highestPriorityCircle);
-//				circleList.remove(highestPriorityCircle);
-//			} else {
-//				break;
-//			}
-//		}
-//		
-//		BufferedImage outputImage = new BufferedImage(width,height, BufferedImage.TYPE_3BYTE_BGR);
-//		
-//		//Draw the circles which have the highest priority
-//		for(Circle curCircle: finalCircleList) {
-//			int x = curCircle.getX();
-//			int y = curCircle.getY();
-//			int radius = curCircle.getRadius();
-//			for(int theta = 0; theta < 360; theta++) {
-//				int a = (int) (x - radius * Math.cos(theta * Math.PI/180));
-//				int b = (int) (y - radius * Math.sin(theta * Math.PI/180));
-//				if((a >= 0 && a < width) && (b >= 0 && b < height))
-//					outputImage.setRGB(a, b, 0xffffff);
-//				else {
-//					outputImage.setRGB(x, y, 0);
-//				}
-//			}
-//		}
-//		
-//		return outputImage;
-//	}
-	
 	//Loads a BufferedImage from a file directory
 	public static BufferedImage loadImage(String imgDir) throws IOException {
 		BufferedImage returnImg = null;
@@ -290,18 +152,6 @@ public abstract class imageFunc {
 			}
 		}
 		
-//		//Debug
-//		{
-//			System.out.println("Debug 1:");
-//			for(int x = 0; x < width; x++) {
-//				System.out.print("{");
-//				for(int y = 0; y < height; y++) {
-//					System.out.print(" " + String.format("%3s",sobelOutput[x][y][0]) + " ,");
-//				}
-//				System.out.print("}\n");
-//			}
-//		}
-		
 		//Calculate the average value for a edge (this just clears up the image)
 		int averageEdge = 0;
 		
@@ -314,24 +164,6 @@ public abstract class imageFunc {
 			
 			averageEdge /= width*height;
 		}
-		
-//		//Debug
-//		{
-//			System.out.println("Debug 2:");
-//			for(int x = 0; x < width; x++) {
-//				System.out.print("{");
-//				for(int y = 0; y < height; y++) {
-//					if(sobelOutput[x][y][0] > averageEdge) {
-//						System.out.print(" " + String.format("%3s",sobelOutput[x][y][0]) + " ,");
-//					} else {
-//						System.out.print(" " + String.format("%3s","0") + " ,");
-//					}
-//				}
-//				System.out.print("}\n");
-//			}
-//		}
-		
-		//System.out.println("Average Edge: " + averageEdge);
 		
 		int[][] centerVoteMatrix = new int[width][height];
 		int[][][] radiusVoteMatrix = new int[width][height][maxRadius];
@@ -363,18 +195,6 @@ public abstract class imageFunc {
 				}
 			}
 		}
-		
-//		//More Debug		
-//		{ 
-//			System.out.println("Debug:");
-//			for(int x = 0; x < width; x++) {
-//				System.out.print("{");
-//				for(int y = 0; y < height; y++) {
-//					System.out.print(" " + String.format("%3s",centerVoteMatrix[x][y]) + " ,");
-//				}
-//				System.out.print("}\n");
-//			}
-//		}
 		
 		//Find the largest value in the array until u run out of circles to find 
 		//Upon finding a circle half the Vote values of the surrounding values in a square of edge length radius/4
@@ -426,6 +246,7 @@ public abstract class imageFunc {
 		BufferedImage outputImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
 		
 		{
+			//For each circle you found draw them
 			for(Circle curCircle : circleList) {
 				for(int theta = 0; theta < 360; theta++) {
 					int x = (int) (curCircle.getX() + curCircle.getRadius() * Math.cos(theta * Math.PI/180));
@@ -436,83 +257,7 @@ public abstract class imageFunc {
 			}
 		}
 		
+		//Finally return the image (that took a while)
 		return outputImage;
 	}
-	
-	
-	//			Private Functions
-	
-
-	//Finds the average red blue and green colour in an image
-	private static Color findAverageColour(BufferedImage img) {
-		int r = 0;
-		int g = 0;
-		int b = 0;
-		
-		for(int x = 0; x < img.getWidth(); x++) {
-			for(int y = 0; y < img.getHeight(); y++) {
-		        int p = img.getRGB(x,y);
-
-		        r += (p>>16)&0xff;
-		        g += (p>>8)&0xff;
-		        b += p&0xff;
-			}
-		}
-		
-		r /= (img.getWidth()*img.getHeight());
-		g /= (img.getWidth()*img.getHeight());
-		b /= (img.getWidth()*img.getHeight());
-		
-		return new Color(r<<16|g<<8|b);
-	}
-	
-	//Tests if a result in the accumulator of a hough matrix result is the highest value against it's neighbours in 3D space
-	private static boolean houghTestMaxima(int[][][] accumulator, int curVoteX, int curVoteY,int curVoteZ, int curVote) {
-		boolean result = false;
-		
-		int[] compareTo = new int[6];
-		
-		/* 0 = x - 1
-		 * 1 = x + 1
-		 * 2 = y - 1
-		 * 3 = y + 1
-		 * 4 = z - 1
-		 * 5 = z + 1
-		 */
-
-		
-		for(int i = 0; i < 6; i++) {
-			try {
-				switch(i) {
-				case 0: compareTo[i] = accumulator[curVoteX - 1][curVoteY][curVoteZ]; break;
-				case 1: compareTo[i] = accumulator[curVoteX + 1][curVoteY][curVoteZ]; break;
-				case 2: compareTo[i] = accumulator[curVoteX][curVoteY - 1][curVoteZ]; break;
-				case 3: compareTo[i] = accumulator[curVoteX][curVoteY + 1][curVoteZ]; break;
-				case 4: compareTo[i] = accumulator[curVoteX][curVoteY][curVoteZ - 1]; break;
-				case 5: compareTo[i] = accumulator[curVoteX][curVoteY][curVoteZ + 1]; break;
-				}
-			} catch (Exception e) {
-				compareTo[i] = 0;
-			}
-		}
-		
-		if(curVote > compareTo[0] && curVote > compareTo[1] && curVote > compareTo[2] && curVote > compareTo[3] && curVote > compareTo[4] && curVote > compareTo[5]) {
-			result = true;
-		}
-		
-		return result;
-	}
-	
-//	//Find the highest priority circle in a collection
-//	private static Circle getHighestPriorityCircle(LinkedList<Circle> circleList) {
-//		Circle returnCircle = new Circle(0,0,0,-1);
-//		
-//		for(Circle curCircle: circleList) {
-//			if(curCircle.getPriority() > returnCircle.getPriority()) {
-//				returnCircle = curCircle;
-//			}
-//		}
-//		
-//		return returnCircle;
-//	}
 }
